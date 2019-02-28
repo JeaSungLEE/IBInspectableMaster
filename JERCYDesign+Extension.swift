5969c04cc65cd40647e2d6a11fbd36c6d325939d
JERCY <wotjdzz1@naver.com>
2019-02-28 18:03:21 +0900

    Update JERCYDesign+Extension.swift

--- JERCYDesign+Extension.swift
+++ JERCYDesign+Extension.swift
@@ -10,7 +10,7 @@ import UIKit
 
 private var flatAssociatedObjectKey: UInt8 = 0
 
-@IBDesignable extension UINavigationBar {
+extension UINavigationBar {
     @IBInspectable var flat: Bool {
         get {
             guard let obj = objc_getAssociatedObject(self, &flatAssociatedObjectKey) as? NSNumber else {
@@ -71,3 +71,53 @@ extension UIView {
         }
     }
 }
+
+extension UIView {
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
